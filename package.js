75f906445e63e84be17958ba8e68476b46f3c645
GABO <gabrielvazquezcivica@gmail.com>
2026-04-28 01:06:32 -0600

    Create package.js

---
+++ package.js
@@ -0,0 +1,34 @@
+{
+  "name": "ultrabot",
+  "version": "2.1.0",
+  "description": "Bot de WhatsApp",
+  "type": "module",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "node --trace-warnings index.js",
+    "install-all": "npm install"
+  },
+  "keywords": [
+    "bot",
+    "whatsapp",
+    "ultrabot",
+    "automatizacion",
+    "rapido",
+    "whatsapp-web",
+    "bot-whatsapp"
+  ],
+  "author": "SoyGabo",
+  "license": "MIT",
+  "dependencies": {
+    "whatsapp-web.js": "^1.26.0",
+    "chalk": "^5.3.0",
+    "qrcode-terminal": "^0.12.0",
+    "figlet": "^1.8.0",
+    "puppeteer": "^24.4.0"
+  },
+  "engines": {
+    "node": ">=16.0.0"
+  }
+}
+  
